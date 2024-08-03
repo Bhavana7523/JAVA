2402e4fc0958278a0595d26f409c83f3ebe3c7f7
Bhavana7523 <130962685+Bhavana7523@users.noreply.github.com>
2024-08-03 14:10:04 +0530

    Create ArithmeticOperators.java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,17 @@
+public class ArithmeticOperators{
+  public static void main(String[] args)
+  {
+    var a=10;
+    var b= 20;
+    var sum = a + b;
+    var difference =  a - b;
+    var product = a * b;
+    var division = a / b;
+    var remainder =  a % b;
+    System.out.println("Sum is"+ sum);
+    System.out.println("Differnece is"+ difference);
+    System.out.println("Product is"+ product);
+    System.out.println("division is"+ division);
+    System.out.println("remainder is"+ remainder);
+  }
+}
